c6a2079c9029c4448c1b7393d134377ef5f31362
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-07-26 22:55:24 +0200

    use rose pine theme

--- lua/core/plugins/theme.lua
+++ lua/core/plugins/theme.lua
@@ -1,10 +1,8 @@
 return {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
+    "rose-pine/neovim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.g.zenbones_darken_comments = 45
-        vim.cmd.colorscheme('zenbones')
+        vim.cmd.colorscheme('rose-pine')
     end
 }
