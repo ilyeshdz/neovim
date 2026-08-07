8f7d42bd76b2494b3d632339a68927b214e68115
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-08-07 22:50:03 +0200

    fixing stuff

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,19 +1,5 @@
 ---@module 'lazy'
 ---@type LazySpec
-
--- Resolve the formatter at runtime based on the project's runtime: Deno
--- projects use `deno fmt`, Bun/Node projects use Prettier.
----@param bufnr integer
----@return string[]?
-local function js_formatters(bufnr)
-  local is_deno = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc', 'deno.lock' }) ~= nil
-  if is_deno then
-    return { 'deno_fmt' }
-  else
-    return { 'prettierd', 'prettier', stop_after_first = true }
-  end
-end
-
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -48,20 +34,12 @@ return {
       },
       -- You can also specify external formatters in here.
       formatters_by_ft = {
-        -- Deno & Bun both use TypeScript/JavaScript, so we pick the formatter
-        -- based on which runtime the project uses (detected by root files) rather
-        -- than by filetype alone.
-        typescript = js_formatters,
-        typescriptreact = js_formatters,
-        javascript = js_formatters,
-        javascriptreact = js_formatters,
-      },
-      formatters = {
-        deno_fmt = {
-          command = 'deno',
-          args = { 'fmt', '-' },
-          stdin = true,
-        },
+        -- rust = { 'rustfmt' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
